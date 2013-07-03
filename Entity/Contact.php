9ea67e838570c35bb876b445901dfdee6b88130d
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2013-07-03 13:55:20 -0400

    Entity

---
+++ Entity/Contact.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace CULabs\ContactBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * CULabs\AdminBundle\Entity\Contact
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="CULabs\ContactBundle\Entity\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $name;
+
+    /**
+     * @var string $email
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @var string $telefono
+     *
+     * @ORM\Column(name="telefono", type="string", length=255)
+     */
+    private $telefono;
+
+    /**
+     * @var string $celular
+     *
+     * @ORM\Column(name="celular", type="string", length=255)
+     */
+    private $celular;
+
+    /**
+     * @var string $ciudad
+     *
+     * @ORM\Column(name="ciudad", type="string", length=255)
+     */
+    private $ciudad;
+
+    /**
+     * @var \CULabs\ContactBundle\Entity\Grupo 
+     * 
+     * @ORM\ManyToMany(targetEntity="Grupo", inversedBy="contacts")
+     */
+    private $grupo;
+    
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->grupo = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Contact
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set telefono
+     *
+     * @param string $telefono
+     * @return Contact
+     */
+    public function setTelefono($telefono)
+    {
+        $this->telefono = $telefono;
+    
+        return $this;
+    }
+
+    /**
+     * Get telefono
+     *
+     * @return string 
+     */
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    /**
+     * Set celular
+     *
+     * @param string $celular
+     * @return Contact
+     */
+    public function setCelular($celular)
+    {
+        $this->celular = $celular;
+    
+        return $this;
+    }
+
+    /**
+     * Get celular
+     *
+     * @return string 
+     */
+    public function getCelular()
+    {
+        return $this->celular;
+    }
+
+    /**
+     * Set ciudad
+     *
+     * @param string $ciudad
+     * @return Contact
+     */
+    public function setCiudad($ciudad)
+    {
+        $this->ciudad = $ciudad;
+    
+        return $this;
+    }
+
+    /**
+     * Get ciudad
+     *
+     * @return string 
+     */
+    public function getCiudad()
+    {
+        return $this->ciudad;
+    }
+    
+    /**
+     * Add grupo
+     *
+     * @param CULabs\ContactBundle\Entity\Grupo $grupo
+     * @return Contact
+     */
+    public function addGrupo(\CULabs\ContactBundle\Entity\Grupo $grupo)
+    {
+        $this->grupo[] = $grupo;
+    
+        return $this;
+    }
+
+    /**
+     * Remove grupo
+     *
+     * @param CULabs\ContactBundle\Entity\Grupo $grupo
+     */
+    public function removeGrupo(\CULabs\ContactBundle\Entity\Grupo $grupo)
+    {
+        $this->grupo->removeElement($grupo);
+    }
+
+    /**
+     * Get grupo
+     *
+     * @return Doctrine\Common\Collections\Collection 
+     */
+    public function getGrupo()
+    {
+        return $this->grupo;
+    }
+}
